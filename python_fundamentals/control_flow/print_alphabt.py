9b3e02a538ade39198febe59a46173265e755b54
aljoud02 <aljoud027@gmail.com>
2026-09-18 20:50:00 +0300

    Add task 2 alphabet script

---
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Script to print the lowercase alphabet except q and e
+alphabet = ""
+
+for i in range(97, 123):
+    letter = chr(i)
+    if letter != 'e' and letter != 'q':
+        alphabet += letter
+
+print(alphabet)
