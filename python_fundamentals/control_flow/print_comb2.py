ad9c84b0fa96be0753869efc0ebddca28df93cec
aljoud02 <aljoud027@gmail.com>
2026-09-18 20:55:40 +0300

    Fix script indentation for task 4

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-# Script to print numbers from 00 to 99 with a single print function
+# Script to print numbers from 00 to 99 with proper indentation
 for i in range(100):
     print("{:02d}".format(i), end=", " if i < 99 else "\n")
